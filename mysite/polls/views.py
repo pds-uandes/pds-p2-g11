7be1c9d4f87cfc54c33cdaa267ef7d27e1479112
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-09-09 16:32:27 -0300

    resultado de si algo esta bien o mal

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-
-# # Create your views here.
-from django.http import HttpResponse
-from .models import Question
-
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -15,10 +13,35 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form with an error message.
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        })
+    
+    # Check if the selected choice is correct
+    if selected_choice.is_correct:
+        result_message = "Your answer is correct!"
+    else:
+        result_message = "Sorry, your answer is incorrect."
+
+    # Increment the vote count for the selected choice
+
+    
+    # Redirect to the results page with a result message
+    return render(request, "polls/results.html", {
+        "question": question,
+        "result_message": result_message,
+    })
+
