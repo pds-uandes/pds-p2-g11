9a26512b341bc3c741e77aba73de161cfeed692b
Dommingo Gonzalez <djgonzalez@miuandes.cl>
2023-09-21 22:11:53 -0300

    Nuevo proyecto

---
+++ mysite/home/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import *
+import random
+from django.http.response import JsonResponse,HttpResponse
+
+# Create your views here.
+
+# {
+#     'data':[
+#         {}
+#     ]
+# }
+
+
+
+def home(request):
+    context = {'categories': Category.objects.all()}
+
+    if request.GET.get('category'):
+        return redirect(f"quiz/?category={request.GET.get('category')}")
+        
+    return render(request, 'home.html',context)
+
+
+def quiz(request):
+    context = {'category': request.GET.get('category')}
+    return render(request, 'quiz.html',context)
+
+
+def get_quiz(request):
+    try:
+        question_objs = Question.objects.all()
+
+        if request.GET.get('category'):
+            question_objs = question_objs.filter(category__category_name__icontains=request.GET.get('category'))
+        question_objs= list(question_objs)
+        data = []
+        random.shuffle(question_objs)
+        for question_obj in question_objs:
+            data.append({
+                "uid": question_obj.uid,
+                "category": question_obj.category.category_name,
+                "question": question_obj.question_text,
+                "marks": question_obj.marks,
+                "answers": question_obj.get_answers()
+            })
+        payload = {'status': True, 'data': data}
+
+        return JsonResponse(payload)
+
+
+    except Exception as e:
+        print(e)
+    return HttpResponse("Something went wrong!")
