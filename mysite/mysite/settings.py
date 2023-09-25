a2f063c1905be0da00d525c67178ee5e61716441
JPLira2 <jplira2@miuandes.cl>
2023-09-24 23:27:03 -0300

    ojala no mueras

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -112,8 +112,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "mystaticfiles"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
